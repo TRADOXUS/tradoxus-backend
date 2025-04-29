465f4860ae20c50b6076f69844299a232922ba93
MPSxDev <mparedes.digital@gmail.com>
2025-04-29 14:23:30 -0400

    fix: Update NFTService

--- src/services/NFTService.ts
+++ src/services/NFTService.ts
@@ -6,6 +6,15 @@ import { Horizon, StellarToml } from "@stellar/stellar-sdk";
 import { isNFT } from "../utils/nft";
 import { redisClient } from "../config/redis";
 
+type StellarAssetResponse = {
+    records: Array<{
+        _links: {
+            self: { href: string };
+            toml?: { href: string };
+        };
+    }>;
+};
+
 export class NFTService extends BaseService<NFT> {
     constructor() {
         super(NFT);
@@ -24,7 +33,7 @@ export class NFTService extends BaseService<NFT> {
             try {
                 account = await server.loadAccount(walletAddress);
             } catch (err) {
-                throw new AppError("Could not get account from provided wallet_address", 404);
+                throw new AppError(404, "Could not get account from provided wallet_address");
             }
             const balances = account.balances;
             for (const balance of balances) {
@@ -40,46 +49,50 @@ export class NFTService extends BaseService<NFT> {
                         image_url: '',
                         owner_address: balance.asset_issuer
                     };
-                    let asset: any;
+                    let assetResponse: StellarAssetResponse;
                     try {
-                        asset = await server
+                        const assetsBuilder = server
                             .assets()
                             .forCode(balance.asset_code)
-                            .forIssuer(balance.asset_issuer)
-                            .call();
+                            .forIssuer(balance.asset_issuer);
+                            
+                        assetResponse = await assetsBuilder.call() as unknown as StellarAssetResponse;
+                        
+                        if (assetResponse.records.length > 0) {
+                            const asset = assetResponse.records[0];
+                            const tomlLink = asset._links.toml?.href;
+                            if (tomlLink) {
+                                try {
+                                    const url = new URL(tomlLink);
+                                    const tomlData = await StellarToml.Resolver.resolve(url.hostname);
+                                    if (!tomlData) {
+                                        throw new Error("Could not resolve toml data");
+                                    }
+                                    const assetMetadata = tomlData.CURRENCIES?.find((c) => c.code === balance.asset_code && c.issuer === balance.asset_issuer);
+                                    if (!assetMetadata) {
+                                        continue;
+                                    }
+                                    if (assetMetadata?.is_asset_anchored && assetMetadata?.anchor_asset_type !== "nft") {
+                                        continue;
+                                    }
+                                    if (!isNFT(assetMetadata)) {
+                                        continue;
+                                    }
+                                    nft = {
+                                        ...nft,
+                                        name: assetMetadata?.name ?? '',
+                                        description: assetMetadata?.desc ?? '',
+                                        image_url: assetMetadata?.image ?? ''
+                                    }
+                                } catch (error) {
+                                    console.log(`Could not get toml metadata for the given asset: ${JSON.stringify(asset)}`);
+                                }
+                            }
+                            nfts.push(nft);
+                        }
                     } catch (err) {
                         console.log(`Could not get assets information for balance: ${JSON.stringify(balance)}`);
                     }
-
-                    const tomlLink = asset?.records[0]._links.toml.href;
-                    if (tomlLink) {
-                        try {
-                            const url = new URL(tomlLink);
-                            const tomlData = await StellarToml.Resolver.resolve(url.hostname);
-                            if (!tomlData) {
-                                throw new Error("Could not resolve toml data");
-                            }
-                            const assetMetadata = tomlData.CURRENCIES?.find((c) => c.code === balance.asset_code && c.issuer === balance.asset_issuer);
-                            if (!assetMetadata) {
-                                continue;
-                            }
-                            if (assetMetadata?.is_asset_anchored && assetMetadata?.anchor_asset_type !== "nft") {
-                                continue;
-                            }
-                            if (!isNFT(assetMetadata)) {
-                                continue;
-                            }
-                            nft = {
-                                ...nft,
-                                name: assetMetadata?.name ?? '',
-                                description: assetMetadata?.desc ?? '',
-                                image_url: assetMetadata?.image ?? ''
-                            }
-                        } catch (error) {
-                            console.log(`Could not get toml metadata for the given asset: ${JSON.stringify(asset)}`);
-                        }
-                    }
-                    nfts.push(nft);
                 }
             }
             await redisClient.set(`nft:${walletAddress}`, JSON.stringify(nfts), {
